aca5aaab37b7be16017106e1de1d7114244b650b
Dmitry Panchuk <10994198@uvu.edu>
2023-09-05 21:05:41 -0600

    First Assignment

---
+++ Lab1a/Lab1a.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Lab1a
+{
+    int num1 = 10; //Created an integer
+    int num2 = 5; //Created a second integer
+    float float1 = 5.4f; //Created a float
+    char character1 = 'C'; //Created a character
+    bool bool1 = true; //Created a boolean
+    string newString = "The answer is letter "; //Created a string
+
+    Console.Writeline(num1 - num2);
+
+    /*	
+     	public Lab1a()
+        {
+
+        }
+    */
+}
