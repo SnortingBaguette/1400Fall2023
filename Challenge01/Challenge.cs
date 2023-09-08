36090922818e7e4f5e256abbcf31f05ae34c4482
Dmitry Panchuk <143127190+SnortingBaguette@users.noreply.github.com>
2023-09-08 17:06:17 -0600

    Completed the Challenge

--- Challenge01/Challenge.cs
+++ Challenge01/Challenge.cs
@@ -4,48 +4,48 @@ public class Program
 {
     public static void Main()
     {
-        string name;
-        int mathAnswer;
-        char answerYesNo;
-        bool likedTheJoke;
-        double doubleQuestion;
+        string name;                     //Initializing the Variables
+        int mathAnswer;                  //Initializing the Variables
+        char answerYesNo;                //Initializing the Variables
+        bool likedTheJoke;               //Initializing the Variables
+        double doubleQuestion;           //Initializing the Variables
 
-        Console.WriteLine("Initializing the Start Up Sequence... OK \nChecking GPU for Mining Capabilities ... OK \nPress Enter To Continue");
-        Console.ReadLine();
+        Console.WriteLine("Initializing the Start Up Sequence... OK \nChecking GPU for Mining Capabilities ... OK \nPress Enter To Continue");   //Sending text on screen
+        Console.ReadLine();        //Waiting for input
         Console.WriteLine("What's your name?");
-        name = Console.ReadLine();
+        name = Console.ReadLine();        //Saving input as a variable
         Console.WriteLine("Yeah, I'll remember that.");
         Console.WriteLine("Wanna Hear a Joke?\nY/N");
-        answerYesNo = Convert.ToChar(Console.ReadLine());
-        if (char.ToUpper(answerYesNo) == 'Y')
+        answerYesNo = Convert.ToChar(Console.ReadLine());        //Converting the input from user to character
+        if (char.ToUpper(answerYesNo) == 'Y')        //Capitalizing the input
         {
             Console.WriteLine("How many developers does it take to screw in a lightbulb?\nNone. It’s a hardware problem. \nHAHAHAHAHAHA \nBy the way, what's 9 + 10?");
-            likedTheJoke = true;
+            likedTheJoke = true;        //Saving the joke status
         }
         else
         {
             Console.WriteLine("Ugh, you're boring. I bet you'd be up for doing some math right now. What's 9 + 10?");
-            likedTheJoke = false;
+            likedTheJoke = false;        //Saving the joke status
             
         }
 
-        mathAnswer = Convert.ToInt32(Console.ReadLine());
+        mathAnswer = Convert.ToInt32(Console.ReadLine());        //Recording the answer
 
         
-        if (mathAnswer == 21) 
+        if (mathAnswer == 21)        //Comparing the values
         {
             Console.WriteLine("You stoopid");
         } 
-        else if (mathAnswer == 19) 
+        else if (mathAnswer == 19)         //Comparing the values
         {
             Console.WriteLine("Welp, math nerd.");
         }
-        else
+        else        //If none are checking out, use this part
         {
             Console.WriteLine("Wrong. Not only you're boring, you're also not very bright, huh?");
         }
 
-        if (likedTheJoke)
+        if (likedTheJoke)        //Comparing joke status
         {
             Console.WriteLine("Well, at least you liked the joke");
         }
@@ -54,8 +54,8 @@ public class Program
             Console.WriteLine("You didn't even like my amazing joke.");
         }
 
-        Console.WriteLine("What's 7/2?");
-        doubleQuestion = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("What's 7/2?");        
+        doubleQuestion = Convert.ToDouble(Console.ReadLine());        //Coverting the answer to float value
         if(doubleQuestion == 3.5)
         {
             Console.WriteLine("Yep, " + name + "\'s a nerd.");
