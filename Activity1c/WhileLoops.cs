f9b48f369483e393df64f3091f5b7e270d1cf4ed
Dmitry Panchuk <143127190+SnortingBaguette@users.noreply.github.com>
2023-09-21 20:42:48 -0600

    While Loops

---
+++ Activity1c/WhileLoops.cs
@@ -0,0 +1,14 @@
+using System;
+					
+public class Program
+{
+	public void Main()
+	{
+		int currentHealth = 26;
+		while(currentHealth < 50)
+		{
+			currentHealth += 1;
+			Console.WriteLine(currentHealth);
+		}
+	}
+}
