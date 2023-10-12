d4ca5f751f591bae09e6e33dcc31723727edcd5d
Dmitry Panchuk <10994198@uvu.edu>
2023-10-11 18:52:14 -0600

    Unit 3 Challenge Completed

--- Unity/Unit3Lab/Assets/Scritpts/MouseLook.cs
+++ Unity/Unit3Lab/Assets/Scritpts/MouseLook.cs
@@ -7,23 +7,23 @@ public class MouseLook : MonoBehaviour
 
     public float mouseSensitivity = 300f;
     public Transform playerBody;
-    float xRotation = 0f;
+    float xRotation = 0f;   //Declare the needed variables
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.lockState = CursorLockMode.Locked;   //Hide the coursor when is play mode
     }
 
     // Update is called once per frame
     void Update()
     {
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;    //Get the mouse input
+        xRotation -= mouseY;    //Rotate the camera over the local X-Axis
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);      //Clamp the rotation to avoid looking up and down too far
 
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        playerBody.Rotate(Vector3.up * mouseX);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);  //Apply the rotation
+        playerBody.Rotate(Vector3.up * mouseX);     //Rotate the player body over the Y-Axis
     
     }
 }
