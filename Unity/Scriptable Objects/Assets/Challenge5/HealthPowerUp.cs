8c485f2f8e00fdfb463ae1480402aa3faf9b0723
Dmitry Panchuk <10994198@uvu.edu>
2023-10-31 21:38:15 -0600

    Attempted Challenge5

---
+++ Unity/Scriptable Objects/Assets/Challenge5/HealthPowerUp.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPowerUp : MonoBehaviour
+{
+
+    [SerializeField] private FloatData healthValue;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            ApplyHealth(other.gameObject);
+            Destroy(gameObject);
+        }
+    }
+
+    private void ApplyHealth(GameObject player)
+    {
+        HealthController healthController = player.GetComponent<HealthController>();
+        if (healthController != null)
+        {
+            healthController.AddHealth(healthValue.Value);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
